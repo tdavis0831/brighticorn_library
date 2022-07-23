851598b9d96dec9de62330e29b656dd0c69191ce
Tasha Davis <tdavis0831@gmail.com>
2022-07-23 15:01:14 -0400

    can run this code without dict, additional changes reflected

--- leet_code_day_3.py
+++ leet_code_day_3.py
@@ -8,17 +8,17 @@
 
 num_listx = [4,3,2,7,8,2,3,1]
 missing_nums_list = []   #hold missing numbers
-nums_dict = {} #convert list to dict to compare keys to i
+# nums_dict = {} #convert list to dict to compare keys to i
  
 def missing_nums(num_list):
     
-    for nums in num_list: #creates the dict
-        nums_dict[nums] = 1
+    # for nums in num_list: #creates the dict
+    #     nums_dict[nums] = 1
 
     
     for i in range(1, len(num_list)+1):  #starting at 1 since we know it starts at 1, and goes to length of num list plus 1 to be includsive
         
-        if i not in nums_dict:              #will keep counting at 1 so as this goes it, it will check to see if in dict keys
+        if i not in num_list:              #will keep counting at 1 so as this goes it, it will check to see if in dict keys
             missing_nums_list.append(i)        #if not present in keys, it will create a new list with item appended
             
     return(missing_nums_list)     #returns the missing items
